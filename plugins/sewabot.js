ba80b30b01a3899b491cdb88afc868dc972a0187
Verdi botz <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-03-13 13:18:24 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,8 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 085706735450
+GoPay: 085706735450
 `.trim()
     const button = {
         buttonText: 'List Harga',
